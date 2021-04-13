f181594f75e65654fbbeabfc6b5b5ed163839a43
KarpukhinAS <KarpukhinAS@tsniimash.ru>
2021-04-13 17:38:07 +0300

    Main form updated.

--- monitoring-client/src/app/general/general.module.ts
+++ monitoring-client/src/app/general/general.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MainFormComponent } from "./main-form/main-form.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ClickOutsideModule } from "ng-click-outside";
 
 @NgModule({
   declarations: [MainFormComponent],
-  imports: [CommonModule, BrowserAnimationsModule],
+  imports: [CommonModule, BrowserAnimationsModule, ClickOutsideModule],
   exports: [MainFormComponent],
 })
 export class GeneralModule {}
--- monitoring-client/src/app/general/main-form/main-form.component.ts
+++ monitoring-client/src/app/general/main-form/main-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { TimeoutError } from "rxjs";
 
 @Component({
   selector: "app-main-form",
@@ -6,7 +7,27 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./main-form.component.css"],
 })
 export class MainFormComponent implements OnInit {
+  isVisible: boolean = true;
+
   constructor() {}
 
   ngOnInit() {}
+
+  onClickedOutside(e: Event): void {
+    console.log(
+      (<HTMLButtonElement>event?.srcElement)?.attributes?.getNamedItem("id")
+        ?.value
+    );
+    this.isVisible =
+      (<HTMLElement>event?.srcElement)?.attributes?.getNamedItem("id")?.value ==
+      "sideBarButton"
+        ? true
+        : false;
+    console.log("Clicked outside:", e);
+  }
+
+  onClicked(e: Event): void {
+    this.isVisible = !this.isVisible;
+    console.log("Clicked:", e);
+  }
 }

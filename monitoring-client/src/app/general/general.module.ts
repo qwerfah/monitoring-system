44c01be87682b5c66ef4f4bdb204e3bc2b687699
qwerfah <qwerfah@gmail.com>
2021-04-20 22:47:58 +0300

    Sidenav added.

--- monitoring-client/src/app/general/general.module.ts
+++ monitoring-client/src/app/general/general.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainFormComponent } from './main-form/main-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [MainFormComponent],
-  imports: [CommonModule, BrowserAnimationsModule],
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatSidenavModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   exports: [MainFormComponent],
 })
 export class GeneralModule {}
--- monitoring-client/src/app/general/main-form/main-form.component.ts
+++ monitoring-client/src/app/general/main-form/main-form.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from "@angular/core";
-import { TimeoutError } from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { TimeoutError } from 'rxjs';
 
 @Component({
-  selector: "app-main-form",
-  templateUrl: "./main-form.component.html",
-  styleUrls: ["./main-form.component.css"],
+  selector: 'app-main-form',
+  templateUrl: './main-form.component.html',
+  styleUrls: ['./main-form.component.css'],
 })
 export class MainFormComponent implements OnInit {
   isVisible: boolean = true;
+  events: string[] = [];
+  opened: boolean = true;
 
   constructor() {}
 
@@ -15,19 +17,19 @@ export class MainFormComponent implements OnInit {
 
   onClickedOutside(e: Event): void {
     console.log(
-      (<HTMLButtonElement>event?.srcElement)?.attributes?.getNamedItem("id")
+      (<HTMLButtonElement>event?.srcElement)?.attributes?.getNamedItem('id')
         ?.value
     );
     this.isVisible =
-      (<HTMLElement>event?.srcElement)?.attributes?.getNamedItem("id")?.value ==
-      "sideBarButton"
+      (<HTMLElement>event?.srcElement)?.attributes?.getNamedItem('id')?.value ==
+      'sideBarButton'
         ? true
         : false;
-    console.log("Clicked outside:", e);
+    console.log('Clicked outside:', e);
   }
 
   onClicked(e: Event): void {
     this.isVisible = !this.isVisible;
-    console.log("Clicked:", e);
+    console.log('Clicked:', e);
   }
 }

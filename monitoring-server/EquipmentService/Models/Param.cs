694ae817da456540ce2ebf1c32277f7e493f785b
qwerfah <qwerfah@gmail.com>
2021-05-09 15:56:21 +0300

    Code cleanup.

--- monitoring-server/EquipmentService/Models/Param.cs
+++ monitoring-server/EquipmentService/Models/Param.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace EquipmentService.Models
+﻿namespace EquipmentService.Models
 {
     /// <summary>
     /// Represents equipment model param.

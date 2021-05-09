21cdf0d7072c14d2ea47e4610b8269dc6124ec33
qwerfah <qwerfah@gmail.com>
2021-05-09 15:17:43 +0300

    Models for monitoring service added.

---
+++ monitoring-server/MonitoringServer/Models/Monitor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonitoringService.Models
+{
+    /// <summary>
+    /// Represents monitor for particular equipment instance.
+    /// Each instance of monitor can track single equipment 
+    /// instance but specified set of instance params.
+    /// </summary>
+    public class Monitor
+    {
+        /// <summary>
+        /// Internal monitor identidier.
+        /// </summary>
+        public long Id { get; set; }
+        /// <summary>
+        /// External monitor identifier.
+        /// </summary>
+        public Guid MonitorUid { get; set; }
+        /// <summary>
+        /// Monitor name.
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Equipment instance external identifier that monitor belongs to.
+        /// </summary>
+        public Guid EquipmentUid { get; set; }
+        /// <summary>
+        /// Equipment model external identifier that monitor belongs to.
+        /// </summary>
+        public Guid EquipmentModelUid { get; set; }
+        /// <summary>
+        /// Navigation property to all tracked params.
+        /// </summary>
+        public virtual List<MonitorParam> MonitorParams { get; set; }
+    }
+}
---
+++ monitoring-server/MonitoringServer/Models/MonitorParam.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonitoringService.Models
+{
+    /// <summary>
+    /// Represents auxiliary instance that describes
+    /// connections between monitors and tracked params.
+    /// </summary>
+    public class MonitorParam
+    {
+        /// <summary>
+        /// Internal monitor identifier.
+        /// </summary>
+        public long MonitorId { get; set; }
+        /// <summary>
+        /// External param identifier.
+        /// </summary>
+        public Guid ParamUid { get; set; }
+        /// <summary>
+        /// Navigation property to monitor instance.
+        /// </summary>
+        public virtual Monitor Monitor { get; set; }
+    }
+}

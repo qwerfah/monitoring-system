7575d7670a5f7e8e7eeaa420ccd4a198142b046f
qwerfah <qwerfah@gmail.com>
2021-05-09 14:46:40 +0300

    Models for equipment service added.

---
+++ monitoring-server/EquipmentService/Models/Equipment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EquipmentService.Models
+{
+    /// <summary>
+    /// Represents particular equipment instance.
+    /// </summary>
+    public class Equipment
+    {
+        /// <summary>
+        /// Internal equipment instance identifier.
+        /// </summary>
+        public long Id { get; set; }
+        /// <summary>
+        /// External equipment instance identifier.
+        /// </summary>
+        public long EquipmentUid { get; set; }
+        /// <summary>
+        /// Equipment model identifier that this instance belongs to.
+        /// </summary>
+        public long EquipmentModelId { get; set; }
+        /// <summary>
+        /// Equipment instance operational status.
+        /// </summary>
+        public EquipmentStatus Status { get; set; }
+        /// <summary>
+        /// Navigation property to equipment model instance.
+        /// </summary>
+        public virtual EquipmentModel EquipmentModel { get; set; }
+    }
+}
---
+++ monitoring-server/EquipmentService/Models/EquipmentStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EquipmentService.Models
+{
+    /// <summary>
+    /// Equimpent instance operational status.
+    /// </summary>
+    public enum EquipmentStatus
+    {
+        Active,
+        Inactive,
+        Decommissioned
+    }
+}

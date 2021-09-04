3ca57000b2c20e9984edf749e13ab4ceef2e75f0
qwerfah <qwerfah@gmail.com>
2021-09-04 17:34:44 +0300

    Main updated

--- monitoring-server/equipment-service/src/main/scala/Main.scala
+++ monitoring-server/equipment-service/src/main/scala/Main.scala
@@ -1,5 +1,5 @@
 import slick.dbio._
-import slick.jdbc.{PostgresProfile, JdbcBackend, JdbcProfile}
+import slick.jdbc.{PostgresProfile, JdbcProfile}
 import slick.jdbc.JdbcBackend.Database
 
 import scala.concurrent.duration.Duration
@@ -15,21 +15,21 @@ import models.DataContext
 
 object Main {
     def main(args: Array[String]): Unit = {
-        val pgdb: JdbcBackend.Database = Database.forConfig("postgres")
+        val pgdb: Database = Database.forConfig("postgres")
         val context: DataContext = new DataContext(PostgresProfile)
 
         val modelRepo = new SlickEquipmentModelRepo(context)
         val instanceRepo = new SlickEquipmentInstanceRepo(context)
         val paramRepo = new SlickParamRepo(context)
 
-        val dBIOTransformer = dBIOTransformation(PostgresProfile, pgdb)
+        val dbManager = new SlickDbManager(pgdb, PostgresProfile)
         val modelService =
             new DefaultEquipmentModelService[Future, DBIO](
               modelRepo,
-              dBIOTransformer
+              dbManager
             )
 
-        Await.result(dBIOTransformer(context.setup), Duration.Inf)
+        Await.result(dbManager.execute(context.setup), Duration.Inf)
 
         println("Schema created")
 
@@ -49,7 +49,7 @@ object Main {
 
     private def dBIOTransformation(
       profile: JdbcProfile,
-      db: JdbcBackend.Database
+      db: Database
     ): DBIO ~> Future = new (DBIO ~> Future) {
         import profile.api._
         override def apply[A](fa: DBIO[A]): Future[A] =

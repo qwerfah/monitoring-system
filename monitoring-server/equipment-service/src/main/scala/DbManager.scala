cad290b0fc6a2372fff866ff5e5cb41a623a5e04
qwerfah <qwerfah@gmail.com>
2021-09-04 17:33:01 +0300

    Db manager trait and impl moved to repos package

--- monitoring-server/equipment-service/src/main/scala/DbManager.scala
+++
@@ -1,5 +0,0 @@
-trait DbManager[F[_], DB[_]] {
-    def execute[A](action: DB[A]): F[A]
-    def executeTransitionally[A](action: DB[A]): F[A]
-    def sequence[A](action: Seq[DB[A]]): DB[Seq[A]]
-}

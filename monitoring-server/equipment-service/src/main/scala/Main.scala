9cfb520d429e03d17ef346b744e18860365d9e9a
qwerfah <qwerfah@gmail.com>
2021-09-05 23:29:10 +0300

    Main updated

--- monitoring-server/equipment-service/src/main/scala/Main.scala
+++ monitoring-server/equipment-service/src/main/scala/Main.scala
@@ -5,31 +5,80 @@ import slick.jdbc.JdbcBackend.Database
 import scala.concurrent.duration.Duration
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.{Await, ExecutionContext, Future}
-import scalaz._
-import scalaz.Scalaz._
-import dbio._
 
+import com.twitter.finagle.{Http, ListeningServer}
+// import com.twitter.util.Future
+
+import cats.effect.{IO, IOApp, Blocker, ExitCode, Resource}
+import cats.effect.syntax._
+
+import io.finch._
+import io.finch.catsEffect._
+import io.finch.circe._
+
+import io.circe.generic.auto._
+
+import scalaz.Monad
+
+import dbio._
 import repos.slick._
 import services.default._
 import models.DataContext
+import com.twitter.finagle.Http
+import cats.effect.IO
+import models.EquipmentModel
+import services.EquipmentModelService
 
-object Main {
+object ModelsController {
+
+    def apply[F[_]: Monad](service: EquipmentModelService[F]) =
+        get("models" :: path[Int]) { modelId: Int =>
+            for { result <- service.getById(modelId) } yield result.get
+        }
+}
+
+object Main extends IOApp {
+
+    val pgdb: Database = Database.forConfig("postgres")
+    val context: DataContext = new DataContext(PostgresProfile)
+
+    val modelRepo = new SlickEquipmentModelRepo(context)
+    val instanceRepo = new SlickEquipmentInstanceRepo(context)
+    val paramRepo = new SlickParamRepo(context)
+
+    val dbManager = new SlickDbManager(pgdb, PostgresProfile)
+
+    import scalaz.Scalaz._
+    val modelService =
+        new DefaultEquipmentModelService[Future, DBIO](
+          modelRepo,
+          dbManager
+        )
+
+    Await.result(dbManager.execute(context.setup), Duration.Inf)
+
+    def startServer: IO[ListeningServer] =
+        IO(
+          Http.server.serve(
+            ":8081",
+            (getModelById)
+                .toServiceAs[Application.Json]
+          )
+        )
+
+    def run(args: List[String]): IO[ExitCode] = {
+        val server = Resource.make(startServer)(s =>
+            IO.suspend(implicitly[ToAsync[Future, IO]].apply(s.close()))
+        )
+
+        server.use(_ => IO.never)
+    }
+}
+
+object db {
+    import scalaz.Scalaz._
+
     def main(args: Array[String]): Unit = {
-        val pgdb: Database = Database.forConfig("postgres")
-        val context: DataContext = new DataContext(PostgresProfile)
-
-        val modelRepo = new SlickEquipmentModelRepo(context)
-        val instanceRepo = new SlickEquipmentInstanceRepo(context)
-        val paramRepo = new SlickParamRepo(context)
-
-        val dbManager = new SlickDbManager(pgdb, PostgresProfile)
-        val modelService =
-            new DefaultEquipmentModelService[Future, DBIO](
-              modelRepo,
-              dbManager
-            )
-
-        Await.result(dbManager.execute(context.setup), Duration.Inf)
 
         println("Schema created")
 

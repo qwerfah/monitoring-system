f37c33e3d3f3175ba36d5f1fb359e5c7e7b519fd
qwerfah <qwerfah@gmail.com>
2021-09-12 14:55:07 +0300

    Imports cleaned

--- monitoring-server/equipment-service/src/main/scala/Main.scala
+++ monitoring-server/equipment-service/src/main/scala/Main.scala
@@ -1,28 +1,18 @@
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
-
-import slick.dbio._
-
 import com.twitter.finagle.{Http, ListeningServer}
 import com.twitter.server.TwitterServer
 import com.twitter.finagle.http.{Request, Response}
 
-import io.finch._
-import io.finch.catsEffect._
-import io.finch.circe._
-
-import io.circe.generic.auto._
-
 import controllers._
-import main.Startup._
 
 object Main extends TwitterServer {
+    import startup.Startup._
+
     startup()
 
     val server =
         Http.serve(
           ":8081",
-          ModelsController.api.toService
+          ModelsController.api
         )
     onExit { server.close() }
 

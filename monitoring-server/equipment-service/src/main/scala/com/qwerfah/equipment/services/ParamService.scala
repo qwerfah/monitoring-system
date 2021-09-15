ef7b9433c925ae3fa31ade566c46e074dd3d0ba7
qwerfah <qwerfah@gmail.com>
2021-09-16 01:29:00 +0300

    Param service defined and implemented

---
+++ monitoring-server/equipment-service/src/main/scala/com/qwerfah/equipment/services/ParamService.scala
@@ -0,0 +1,15 @@
+package com.qwerfah.equipment.services
+
+import com.qwerfah.equipment.models._
+import com.qwerfah.equipment.resources._
+
+trait ParamService[F[_]] {
+    def get: F[ServiceResponse[Seq[ParamResponse]]]
+    def getById(id: Int): F[ServiceResponse[ParamResponse]]
+    def getByUid(uid: Uid): F[ServiceResponse[ParamResponse]]
+    def getByModelUid(modelUid: Uid): F[ServiceResponse[Seq[ParamResponse]]]
+    def add(param: AddParamRequest): F[ServiceResponse[ParamResponse]]
+    def update(uid: Uid, param: UpdateParamRequest): F[ServiceResponse[String]]
+    def removeById(id: Int): F[ServiceResponse[String]]
+    def removeByUid(uid: Uid): F[ServiceResponse[String]]
+}
---
+++ monitoring-server/equipment-service/src/main/scala/com/qwerfah/equipment/services/default/DefaultParamService.scala
@@ -0,0 +1,86 @@
+package com.qwerfah.equipment.services.default
+
+import cats.Monad
+import cats.implicits._
+
+import com.qwerfah.equipment.repos._
+import com.qwerfah.equipment.models._
+import com.qwerfah.equipment.services._
+import com.qwerfah.equipment.resources._
+import com.qwerfah.equipment.Mappings
+
+class DefaultParamService[F[_]: Monad, DB[_]: Monad](implicit
+  modelRepo: EquipmentModelRepo[DB],
+  paramRepo: ParamRepo[DB],
+  dbManager: DbManager[F, DB]
+) extends ParamService[F] {
+    import Mappings._
+
+    override def get: F[ServiceResponse[Seq[ParamResponse]]] = for {
+        params <- dbManager.execute(paramRepo.get)
+    } yield ServiceResult(params)
+
+    override def getById(id: Int): F[ServiceResponse[ParamResponse]] = for {
+        result <- dbManager.execute(paramRepo.getById(id))
+    } yield result match {
+        case Some(param) =>
+            ServiceResult(param)
+        case None => ServiceEmpty
+    }
+
+    override def getByUid(uid: Uid): F[ServiceResponse[ParamResponse]] =
+        for {
+            result <- dbManager.execute(paramRepo.getByUid(uid))
+        } yield result match {
+            case Some(param) =>
+                ServiceResult(param)
+            case None => ServiceEmpty
+        }
+
+    override def getByModelUid(
+      modelUid: Uid
+    ): F[ServiceResponse[Seq[ParamResponse]]] = for {
+        params <- dbManager.execute(paramRepo.getByModelUid(modelUid))
+    } yield ServiceResult(params)
+
+    override def add(
+      request: AddParamRequest
+    ): F[ServiceResponse[ParamResponse]] = {
+        dbManager.execute(modelRepo.getByUid(request.modelUid)) flatMap {
+            case Some(model) =>
+                dbManager.execute(paramRepo.add(request)) map { param =>
+                    ServiceResult(param)
+                }
+            case None => Monad[F].pure(ServiceEmpty)
+        }
+    }
+
+    override def update(
+      uuid: Uid,
+      request: UpdateParamRequest
+    ): F[ServiceResponse[String]] = {
+        val param: Param = request
+        for {
+            result <- dbManager.execute(
+              paramRepo.update(param.copy(uid = uuid))
+            )
+        } yield result match {
+            case 1 => ServiceResult("Param updated")
+            case _ => ServiceEmpty
+        }
+    }
+
+    override def removeById(id: Int): F[ServiceResponse[String]] = for {
+        result <- dbManager.execute(paramRepo.removeById(id))
+    } yield result match {
+        case 1 => ServiceResult("Param removed")
+        case _ => ServiceEmpty
+    }
+
+    override def removeByUid(uid: Uid): F[ServiceResponse[String]] = for {
+        result <- dbManager.execute(paramRepo.removeByUid(uid))
+    } yield result match {
+        case 1 => ServiceResult("Param removed")
+        case _ => ServiceEmpty
+    }
+}

bc71d0354ba0fed973cd9637aae83a2f6511fc61
qwerfah <qwerfah@gmail.com>
2021-09-17 12:56:51 +0300

    Session api resources added

---
+++ monitoring-server/session-service-api/build.sbt
@@ -0,0 +1,5 @@
+name := "session-service-api"
+
+version := "1.0"
+
+scalaVersion := "2.12.14"
---
+++ monitoring-server/session-service-api/src/main/scala/com/qwerfah/session/resources/UserRole.scala
@@ -0,0 +1,23 @@
+package com.qwerfah.session.resources
+
+import enumeratum._
+
+sealed trait UserRole extends EnumEntry
+
+/** System yser role. */
+case object UserRole extends Enum[UserRole] with CirceEnum[UserRole] {
+
+    /** Sysytem administrator (access to all operations). */
+    case object SystemAdmin extends UserRole
+
+    /** Equipment administrator (access to all equipment data manipulation
+      * operations).
+      */
+    case object EquipmentAdmin extends UserRole
+
+    /** Equipment user (access only to equipment data retrieving operations). */
+    case object EquipmentUser extends UserRole
+
+    val values = findValues
+
+}

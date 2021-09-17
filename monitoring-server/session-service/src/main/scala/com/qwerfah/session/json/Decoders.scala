7d1c25dcb9efbc314055e4e17b6506a845c5534b
qwerfah <qwerfah@gmail.com>
2021-09-17 20:40:50 +0300

    Json decoders for session service resources added

---
+++ monitoring-server/session-service/src/main/scala/com/qwerfah/session/json/Decoders.scala
@@ -0,0 +1,43 @@
+package com.qwerfah.session.json
+
+import io.circe.{Decoder, HCursor}
+
+import cats.data.Validated._
+import cats.data.{NonEmptyList, Validated, ValidatedNel}
+import cats.implicits._
+
+import com.qwerfah.common.Exceptions._
+import com.qwerfah.common.Uid
+import com.qwerfah.common.ErrorMessages._
+import com.qwerfah.session.resources._
+
+object Decoders {
+    import JsonSchemas._
+
+    implicit val decodeUserRequest: Decoder[UserRequest] =
+        (c: HCursor) => {
+            userRequestSchema.validate(c.value) match {
+                case Valid(()) =>
+                    for {
+                        login <- c.downField("login").as[String]
+                        password <- c.downField("password").as[String]
+                        role <- c.downField("role").as[UserRole]
+                    } yield UserRequest(login, password, role)
+                case Invalid(errors) =>
+                    throw InvalidJsonBodyException(ValidationError, errors)
+            }
+        }
+
+    implicit val decodeCredentials: Decoder[Credentials] =
+        (c: HCursor) => {
+            userRequestSchema.validate(c.value) match {
+                case Valid(()) =>
+                    for {
+                        login <- c.downField("login").as[String]
+                        password <- c.downField("password").as[String]
+                    } yield Credentials(login, password)
+                case Invalid(errors) =>
+                    throw InvalidJsonBodyException(ValidationError, errors)
+            }
+        }
+}

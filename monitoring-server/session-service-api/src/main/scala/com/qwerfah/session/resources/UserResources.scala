a75fc76346808a6284acc7f834ec682d43de6311
qwerfah <qwerfah@gmail.com>
2021-09-17 20:42:22 +0300

    Session service api resources updated

--- monitoring-server/session-service-api/src/main/scala/com/qwerfah/session/resources/UserResources.scala
+++ monitoring-server/session-service-api/src/main/scala/com/qwerfah/session/resources/UserResources.scala
@@ -2,10 +2,46 @@ package com.qwerfah.session.resources
 
 import com.qwerfah.common.Uid
 
-final case class UserRequest(login: String, password: String, role: UserRole)
+/** Register new or update exisiting user request data.
+  * @param login
+  *   User login.
+  * @param password
+  *   User password.
+  * @param role
+  *   User role.
+  */
+final case class UserRequest(
+  login: String,
+  password: String,
+  role: UserRole
+)
 
+/** User credentials.
+  * @param login
+  *   User login.
+  * @param password
+  *   User password.
+  */
+final case class Credentials(login: String, password: String)
+
+/** User data returned by service.
+  * @param uid
+  *   User uid.
+  * @param login
+  *   User login.
+  * @param role
+  *   User role.
+  */
 final case class UserResponse(
   uid: Uid,
   login: String,
   role: UserRole
 )
+
+/** User jwt tokens.
+  * @param access
+  *   Access token.
+  * @param refresh
+  *   Refresh token.
+  */
+final case class Token(access: String, refresh: String)

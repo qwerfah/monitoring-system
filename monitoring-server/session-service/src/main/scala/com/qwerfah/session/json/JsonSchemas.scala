530b91f2022b1e11df1842ddb2bc8ee32eae673b
qwerfah <qwerfah@gmail.com>
2021-09-17 20:40:32 +0300

    Json schemas for session service api resources added

---
+++ monitoring-server/session-service/src/main/scala/com/qwerfah/session/json/JsonSchemas.scala
@@ -0,0 +1,52 @@
+package com.qwerfah.session.json
+
+import io.circe.literal._
+import io.circe.schema.Schema
+
+object JsonSchemas {
+    val userRequestSchema: Schema = Schema.load(
+      json""" {
+        "type": "object",
+        "required": ["login", "password", "role"],
+        "properties": {
+            "login": {
+                "type": "string",
+                "minLength": 1,
+                "maxLength": 100,
+                "pattern": "^(?!\\s*$$).+"
+            },
+            "password": {
+                "type": "string",
+                "minLength": 1,
+                "maxLength": 100,
+                "pattern": "^(?!\\s*$$).+"
+            },
+            "role": {
+                "type": "string",
+                "enum": ["SystemAdmin", "EquipmentAdmin", "EquipmentUser"]
+            }
+        }  
+    }"""
+    )
+
+    val credentialsSchema: Schema = Schema.load(
+      json""" {
+        "type": "object",
+        "required": ["login", "password"],
+        "properties": {
+            "login": {
+                "type": "string",
+                "minLength": 1,
+                "maxLength": 100,
+                "pattern": "^(?!\\s*$$).+"
+            },
+            "password": {
+                "type": "string",
+                "minLength": 1,
+                "maxLength": 100,
+                "pattern": "^(?!\\s*$$).+"
+            }
+        }  
+    }"""
+    )
+}

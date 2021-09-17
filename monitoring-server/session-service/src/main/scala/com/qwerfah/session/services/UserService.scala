6d529a4139198ec796140bcef1d3518e507a6998
qwerfah <qwerfah@gmail.com>
2021-09-17 20:41:43 +0300

    User service trait updated

--- monitoring-server/session-service/src/main/scala/com/qwerfah/session/services/UserService.scala
+++ monitoring-server/session-service/src/main/scala/com/qwerfah/session/services/UserService.scala
@@ -5,12 +5,52 @@ import com.qwerfah.session.resources._
 import com.qwerfah.common.services.ServiceResponse
 import com.qwerfah.common.Uid
 
+/** Describe user service funcionality. */
 trait UserService[F[_]] {
-    def register(user: UserRequest): F[ServiceResponse[UserResponse]]
-    def login(login: String, password: String): F[ServiceResponse[UserResponse]]
-    def refresh()
+
+    /** Register new user.
+      * @param request
+      *   User data.
+      * @return
+      *   Registered user data.
+      */
+    def register(request: UserRequest): F[ServiceResponse[UserResponse]]
+
+    /** Authorize user with given credentials.
+      * @param credentials
+      *   User credentials.
+      * @return
+      *   User tokens.
+      */
+    def login(credentials: Credentials): F[ServiceResponse[Token]]
+
+    /** Refresh user tokens using refresh token.
+      * @param token
+      *   Refresh token (not expired).
+      * @return
+      *   New access and refresh tokens.
+      */
+    def refresh(token: String): F[ServiceResponse[Token]]
+
+    /** Get all registered users.
+      * @return
+      *   All registered users.
+      */
     def getAll: F[ServiceResponse[Seq[UserResponse]]]
+
+    /** Get registered user by its uid.
+      * @param uid
+      *   User uid.
+      * @return
+      *   Registered user with given uid.
+      */
     def get(uid: Uid): F[ServiceResponse[UserResponse]]
-    def update(uid: Uid, user: UserRequest): F[ServiceResponse[String]]
+
+    /** Update registered user by its uid.
+     @param uid User uid.
+      *   @param request New user data.
+      * @return
+      */
+    def update(uid: Uid, request: UserRequest): F[ServiceResponse[String]]
     def remove(uid: Uid): F[ServiceResponse[String]]
 }

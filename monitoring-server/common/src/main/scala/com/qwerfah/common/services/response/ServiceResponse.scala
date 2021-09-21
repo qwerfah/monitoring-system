0238088a0e1ea4cb692c0ce38d269ee04ec043b1
qwerfah <qwerfah@gmail.com>
2021-09-21 18:44:23 +0300

    Error service responses extended

--- monitoring-server/common/src/main/scala/com/qwerfah/common/services/response/ServiceResponse.scala
+++ monitoring-server/common/src/main/scala/com/qwerfah/common/services/response/ServiceResponse.scala
@@ -11,16 +11,67 @@ sealed trait ServiceResponse[+T]
   */
 final case class ObjectResponse[T](result: T) extends ServiceResponse[T]
 
-/** Represent service method invocation which ends with failure.
+/** Represent result of service method invocation which ends with failure.
   * @param error
   *   Error instance.
   */
 sealed trait ErrorResponse extends Exception with ServiceResponse[Nothing]
 
+/** Returns when service operation can't can't get the required resource.
+  * Corresponds to 404 status code.
+  * @param message
+  *   Error description.
+  */
 final case class NotFoundResponse(message: ErrorMessage) extends ErrorResponse {
     override def getMessage = message.getMessage
 }
 
+/** Returns when authorization operation results in error. Corresponds to 400
+  * status code.
+  * @param message
+  *   Error description.
+  */
 final case class BadAuthResponse(message: ErrorMessage) extends ErrorResponse {
+    override def getMessage = message.getMessage
+}
+
+/** Returns when the request to the remote server inside of service method
+  * invocation ends with 503 status code. Corresponds to 502 status code.
+  * @param message
+  *   Error description.
+  */
+final case class BadGatewayResponse(message: ErrorMessage)
+  extends ErrorResponse {
+    override def getMessage = message.getMessage
+}
+
+/** Returns when service operation ends with unprocessed error. Corresponds to
+  * 500 status code.
+  * @param message
+  *   Error description.
+  */
+final case class InternalErrorResponse(message: ErrorMessage)
+  extends ErrorResponse {
+    override def getMessage = message.getMessage
+}
+
+/** Returns when the successful request to the remote server inside of service
+  * method invocation returns result that does not corresponds to expected.
+  * Corresponds to 422 status code.
+  * @param message
+  *   Error description.
+  */
+final case class UnprocessableResponse(message: ErrorMessage)
+  extends ErrorResponse {
+    override def getMessage = message.getMessage
+}
+
+/** Returns when the request to the remote server inside of service method
+  * invocation returns unexpected status code. Corresponds to 520 status code.
+  * @param message
+  *   Error description.
+  */
+final case class UnknownErrorResponse(message: ErrorMessage)
+  extends ErrorResponse {
     override def getMessage = message.getMessage
 }

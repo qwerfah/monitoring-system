9c292ea31ffb827f69948556c1c6fafaba91d118
qwerfah <qwerfah@gmail.com>
2021-09-23 00:24:23 +0300

    Monitoring service repos implemented

---
+++ monitoring-server/monitoring-service/src/main/scala/com/qwerfah/monitoring/repos/slick/SlickMonitorParamRepo.scala
@@ -0,0 +1,46 @@
+package com.qwerfah.monitoring.repos.slick
+
+import slick.dbio._
+import slick.jdbc.JdbcProfile
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import com.qwerfah.monitoring.repos.MonitorParamRepo
+import com.qwerfah.monitoring.models._
+import com.qwerfah.common.Uid
+
+class SlickMonitorParamRepo(implicit val context: MonitoringContext)
+  extends MonitorParamRepo[DBIO] {
+    import context.profile.api._
+
+    override def get: DBIO[Seq[MonitorParam]] = context.monitorParams.result
+
+    override def get(monitorParam: MonitorParam): DBIO[Option[MonitorParam]] =
+        context.monitorParams
+            .filter(p =>
+                p.paramUid === monitorParam.paramUid &&
+                p.paramUid === monitorParam.monitorUid
+            )
+            .result
+            .headOption
+
+    override def getByMonitorUid(monitorUid: Uid): DBIO[Seq[MonitorParam]] =
+        context.monitorParams.filter(_.monitorUid === monitorUid).result
+
+    override def getByParamUid(paramUid: Uid): DBIO[Seq[MonitorParam]] =
+        context.monitorParams.filter(_.paramUid === paramUid).result
+
+    override def add(monitorParam: MonitorParam): DBIO[Int] =
+        context.monitorParams += monitorParam
+
+    override def removeByParamUid(paramUid: Uid): DBIO[Int] =
+        context.monitorParams.filter(_.paramUid === paramUid).delete
+
+    override def remove(monitorParam: MonitorParam): DBIO[Int] =
+        context.monitorParams
+            .filter(p =>
+                p.paramUid === monitorParam.paramUid &&
+                p.paramUid === monitorParam.monitorUid
+            )
+            .delete
+
+}
---
+++ monitoring-server/monitoring-service/src/main/scala/com/qwerfah/monitoring/repos/slick/SlickMonitorRepo.scala
@@ -0,0 +1,46 @@
+package com.qwerfah.monitoring.repos.slick
+
+import slick.dbio._
+import slick.jdbc.JdbcProfile
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import com.qwerfah.monitoring.repos.MonitorRepo
+import com.qwerfah.monitoring.models._
+import com.qwerfah.common.Uid
+
+class SlickMonitorRepo(implicit val context: MonitoringContext)
+  extends MonitorRepo[DBIO] {
+    import context.profile.api._
+
+    override def get: DBIO[Seq[Monitor]] = context.monitors.result
+
+    override def get(id: Int): DBIO[Option[Monitor]] =
+        context.monitors.filter(_.id === id).result.headOption
+
+    override def get(uid: Uid): DBIO[Option[Monitor]] =
+        context.monitors.filter(_.uid === uid).result.headOption
+
+    override def getByInstanceUid(instanceUid: Uid): DBIO[Seq[Monitor]] =
+        context.monitors.filter(_.instanceUid === instanceUid).result
+
+    override def add(monitor: Monitor): DBIO[Monitor] =
+        (context.monitors returning context.monitors.map(_.id) into (
+          (param, id) => param.copy(id = Some(id))
+        )) += monitor
+
+    override def update(monitor: Monitor): DBIO[Int] = {
+        val targetRows = context.monitors.filter(_.uid === monitor.uid)
+        for {
+            old <- targetRows.result.headOption
+            updateActionOption = old.map(b => targetRows.update(monitor))
+            affected <- updateActionOption.getOrElse(DBIO.successful(0))
+        } yield affected
+    }
+
+    override def removeById(id: Int): DBIO[Int] =
+        context.monitors.filter(_.id === id).delete
+
+    override def removeByUid(uid: Uid): DBIO[Int] =
+        context.monitors.filter(_.uid === uid).delete
+
+}

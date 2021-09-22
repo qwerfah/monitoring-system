947c81f04011e4ed564ec23cbb347c9c464e8a98
qwerfah <qwerfah@gmail.com>
2021-09-23 00:23:59 +0300

    Monitoring service repos added

---
+++ monitoring-server/monitoring-service/src/main/scala/com/qwerfah/monitoring/repos/MonitorParamRepo.scala
@@ -0,0 +1,14 @@
+package com.qwerfah.monitoring.repos
+
+import com.qwerfah.monitoring.models.MonitorParam
+import com.qwerfah.common.Uid
+
+trait MonitorParamRepo[DB[_]] {
+    def get: DB[Seq[MonitorParam]]
+    def get(monitor: MonitorParam): DB[Option[MonitorParam]]
+    def getByMonitorUid(monitorUid: Uid): DB[Seq[MonitorParam]]
+    def getByParamUid(paramUid: Uid): DB[Seq[MonitorParam]]
+    def add(monitor: MonitorParam): DB[Int]
+    def removeByParamUid(paramUid: Uid): DB[Int]
+    def remove(monitor: MonitorParam): DB[Int]
+}
---
+++ monitoring-server/monitoring-service/src/main/scala/com/qwerfah/monitoring/repos/MonitorRepo.scala
@@ -0,0 +1,15 @@
+package com.qwerfah.monitoring.repos
+
+import com.qwerfah.monitoring.models.Monitor
+import com.qwerfah.common.Uid
+
+trait MonitorRepo[DB[_]] {
+    def get: DB[Seq[Monitor]]
+    def get(id: Int): DB[Option[Monitor]]
+    def get(uid: Uid): DB[Option[Monitor]]
+    def getByInstanceUid(instanceUid: Uid): DB[Seq[Monitor]]
+    def add(monitor: Monitor): DB[Monitor]
+    def update(monitor: Monitor): DB[Int]
+    def removeById(id: Int): DB[Int]
+    def removeByUid(uid: Uid): DB[Int]
+}

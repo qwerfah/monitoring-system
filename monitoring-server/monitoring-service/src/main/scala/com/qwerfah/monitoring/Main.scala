afe08091f8eed5e7326764bdcddeaed05d8613e5
qwerfah <qwerfah@gmail.com>
2021-09-25 00:50:00 +0300

    Startup settings for monitoring service added

--- monitoring-server/monitoring-service/src/main/scala/com/qwerfah/monitoring/Main.scala
+++ monitoring-server/monitoring-service/src/main/scala/com/qwerfah/monitoring/Main.scala
@@ -1,3 +1,34 @@
-object Main extends App {
-    println("monitor")
+package com.qwerfah.monitoring
+
+import com.twitter.finagle.{Http, ListeningServer}
+import com.twitter.server.TwitterServer
+import com.twitter.finagle.http.{Request, Response}
+
+import io.finch.Application
+import io.finch.circe._
+import io.circe.generic.auto._
+
+import com.qwerfah.monitoring.controllers._
+import com.qwerfah.common.json.Encoders
+import com.twitter.finagle.SimpleFilter
+
+import com.qwerfah.common.controllers.RequestLoggingFilter
+
+object Main extends TwitterServer {
+    import Startup._
+    import Encoders._
+
+    startup()
+
+    val server =
+        Http.serve(
+          ":8083",
+          RequestLoggingFilter.andThen(
+            MonitorController.api
+                .toServiceAs[Application.Json]
+          )
+        )
+    onExit { server.close() }
+
+    com.twitter.util.Await.ready(server)
 }
---
+++ monitoring-server/monitoring-service/src/main/scala/com/qwerfah/monitoring/Startup.scala
@@ -0,0 +1,48 @@
+package com.qwerfah.monitoring
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import com.rms.miu.slickcats.DBIOInstances._
+import com.twitter.util.{Future, Await}
+import io.catbird.util._
+
+import slick.jdbc.{PostgresProfile, JdbcProfile}
+import slick.jdbc.JdbcBackend.Database
+import slick.dbio._
+
+import com.qwerfah.monitoring.repos.slick._
+import com.qwerfah.monitoring.services.default._
+import com.qwerfah.monitoring.models.MonitoringContext
+import com.qwerfah.monitoring.repos.MonitorRepo
+import com.qwerfah.common.http._
+import com.qwerfah.common.resources.Credentials
+import com.qwerfah.common.db.slick.SlickDbManager
+import com.qwerfah.common.repos.local.LocalTokenRepo
+import com.qwerfah.common.repos.slick.SlickUserRepo
+import com.qwerfah.common.services.default._
+
+object Startup {
+    implicit val pgdb = Database.forConfig("postgres")
+    implicit val dbProfile = PostgresProfile
+    implicit val context = new MonitoringContext
+
+    implicit val monitorRepo = new SlickMonitorRepo
+    implicit val monitorParamRepo = new SlickMonitorParamRepo
+    implicit val tokenRepo = new LocalTokenRepo
+    implicit val userRepo = new SlickUserRepo
+    implicit val dbManager = new SlickDbManager
+
+    val defaultEquipmentClient =
+        new DefaultHttpClient(
+          Equipment,
+          Credentials("monitoring", "monitoring"),
+          "localhost:8081"
+        )
+
+    implicit val DefaultEquipmentService =
+        new DefaultMonitorService[Future, DBIO](defaultEquipmentClient)
+    implicit val defaultTokenService = new DefaultTokenService[Future, DBIO]
+    implicit val defaultUserService = new DefaultUserService[Future, DBIO]
+
+    def startup() = Await.result(dbManager.execute(context.setup))
+}

f8baf0dcd88cdf4fa70862f13d1f6b3aa6e68f07
qwerfah <qwerfah@gmail.com>
2021-09-25 00:50:47 +0300

    Default monitor service implementation updated

--- monitoring-server/monitoring-service/src/main/scala/com/qwerfah/monitoring/services/default/DefaultMonitorService.scala
+++ monitoring-server/monitoring-service/src/main/scala/com/qwerfah/monitoring/services/default/DefaultMonitorService.scala
@@ -1,32 +1,64 @@
 package com.qwerfah.monitoring.services.default
 
-import cats.Monad
+import cats.{Monad, Applicative}
 import cats.implicits._
 
 import com.qwerfah.monitoring.services._
 import com.qwerfah.monitoring.repos._
 import com.qwerfah.monitoring.models._
 import com.qwerfah.monitoring.resources._
+import com.qwerfah.monitoring.Mappings
 import com.qwerfah.equipment.resources._
+import com.qwerfah.equipment.json.Decoders
 import com.qwerfah.common.db.DbManager
 import com.qwerfah.common.services.response._
 import com.qwerfah.common.Uid
+import com.qwerfah.common.exceptions.NoMonitor
+import com.qwerfah.common.http._
 
-class DefaultMonitorService[DB[_]: Monad, F[_]: Monad](
+class DefaultMonitorService[F[_]: Monad, DB[_]: Monad](
+  client: HttpClient[F]
+)(implicit
   monitorRepo: MonitorRepo[DB],
   monitorParamRepo: MonitorParamRepo[DB],
   dbManager: DbManager[F, DB]
 ) extends MonitorService[F] {
-    override def get: F[ServiceResponse[Seq[MonitorResponse]]] = ???
+    import Mappings._
+    import Decoders._
 
-    override def get(uid: Uid): F[ServiceResponse[MonitorResponse]] = ???
+    override def get: F[ServiceResponse[Seq[MonitorResponse]]] =
+        dbManager.execute(monitorRepo.get) map { ObjectResponse(_) }
+
+    override def get(uid: Uid): F[ServiceResponse[MonitorResponse]] =
+        dbManager.execute(monitorRepo.get(uid)) map {
+            case Some(value) => ObjectResponse(value)
+            case None        => NotFoundResponse(NoMonitor(uid))
+        }
 
     override def getByInstanceUid(
       instanceUid: Uid
-    ): F[ServiceResponse[Seq[MonitorResponse]]] = ???
+    ): F[ServiceResponse[Seq[MonitorResponse]]] =
+        dbManager.execute(monitorRepo.getByInstanceUid(instanceUid)) map {
+            ObjectResponse(_)
+        }
 
-    override def getParams(uid: Uid): F[ServiceResponse[Seq[ParamResponse]]] =
-        ???
+    override def getParams(
+      uid: Uid
+    ): F[ServiceResponse[Seq[ParamResponse]]] = {
+        dbManager.execute(monitorParamRepo.getByMonitorUid(uid)) flatMap {
+            _.map(p =>
+                client
+                    .sendAndDecode[ParamResponse](Get, s"/params/${p.paramUid}")
+            ).sequence map { params =>
+                params.foldLeft(Seq[ParamResponse]()) { (a, b) =>
+                    b match {
+                        case ObjectResponse(value) => a.appended(value)
+                        case other                 => a
+                    }
+                }
+            } map { ObjectResponse(_) }
+        }
+    }
 
     override def add(
       request: MonitorRequest

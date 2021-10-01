44c375854d03e3939985d2b8195d4949a8d44a48
qwerfah <qwerfah@gmail.com>
2021-10-01 19:07:31 +0300

    Model files filtering added, methods return file meta collections now

--- monitoring-server/documentation-service/src/main/scala/com/qwerfah/documentation/repos/slick/SlickFileRepo.scala
+++ monitoring-server/documentation-service/src/main/scala/com/qwerfah/documentation/repos/slick/SlickFileRepo.scala
@@ -4,26 +4,39 @@ import slick.dbio._
 import slick.jdbc.JdbcProfile
 import scala.concurrent.ExecutionContext.Implicits.global
 
+import com.qwerfah.documentation.models._
 import com.qwerfah.documentation.repos.FileRepo
-import com.qwerfah.documentation.models.DocumentationContext
 
 import com.qwerfah.common.Uid
 import com.qwerfah.common.http.HttpMethod
-import com.qwerfah.documentation.models.File
 
 class SlickFileRepo(implicit val context: DocumentationContext)
   extends FileRepo[DBIO] {
     import context.profile.api._
 
-    override def get: DBIO[Seq[File]] = context.files.result
+    override def getMeta: DBIO[Seq[FileMeta]] = context.files.result.map(
+      _.map(f => FileMeta(f.id, f.uid, f.modelUid, f.filename, f.contentType))
+    )
+
+    override def getModelMeta(modelUid: Uid): DBIO[Seq[FileMeta]] =
+        context.files
+            .filter(_.modelUid === modelUid)
+            .result
+            .map(
+              _.map(f =>
+                  FileMeta(f.id, f.uid, f.modelUid, f.filename, f.contentType)
+              )
+            )
 
     override def get(uid: Uid): DBIO[Option[File]] =
         context.files.filter(_.uid === uid).result.headOption
 
-    override def add(file: File): DBIO[File] =
+    override def add(file: File): DBIO[FileMeta] =
         (context.files returning context.files.map(
           _.id
-        ) into ((f, id) => f.copy(id = Some(id)))) += file
+        ) into ((f, id) =>
+            FileMeta(Some(id), f.uid, f.modelUid, f.filename, f.contentType)
+        )) += file
 
     override def remove(uid: Uid): DBIO[Int] =
         context.files.filter(_.uid === uid).delete

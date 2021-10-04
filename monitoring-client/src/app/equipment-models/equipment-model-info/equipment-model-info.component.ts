02bdae6c4f11419e1ec05be54014cd17d7b9d28d
qwerfah <qwerfah@gmail.com>
2021-10-04 16:48:56 +0300

    Model info component created

---
+++ monitoring-client/src/app/equipment-models/equipment-model-info/equipment-model-info.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { v4 as uuid } from 'uuid';
+
+import { EquipmentService } from 'src/app/services/equipment.service';
+
+import { EquipmentModel } from 'src/app/models/equipment-model';
+import { Param } from 'src/app/models/param';
+import { EquipmentInstance } from 'src/app/models/equipment-instance';
+import { InstanceStatus } from 'src/app/models/instance-status';
+
+@Component({
+  selector: 'app-equipment-model-info',
+  templateUrl: './equipment-model-info.component.html',
+  styleUrls: ['./equipment-model-info.component.css'],
+})
+export class EquipmentModelInfoComponent implements OnInit {
+  modelUid: string;
+  model$: Observable<EquipmentModel>;
+  params$: Observable<Param[]>;
+  instances$: Observable<EquipmentInstance[]>;
+
+  constructor(private route: ActivatedRoute, private equipmentService: EquipmentService) {}
+
+  ngOnInit() {
+    this.modelUid = this.route.snapshot.params['uid'];
+    this.model$ = of(
+      new EquipmentModel(
+        uuid(),
+        'model_1',
+        'description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1 description_1'
+      )
+    ); //this.equipmentService.getModel(this.modelUid);
+
+    this.params$ = of([
+      new Param(uuid(), 'Length', 'm'),
+      new Param(uuid(), 'Heigth', 'm'),
+      new Param(uuid(), 'Width', 'm'),
+      new Param(uuid(), 'Weigth', 'kg'),
+      new Param(uuid(), 'Speed', 'm/s'),
+    ]);
+
+    this.instances$ = of([
+      new EquipmentInstance(uuid(), uuid(), 'instance1', 'description of instance 1', InstanceStatus.Active),
+      new EquipmentInstance(uuid(), uuid(), 'instance2', 'description of instance 2', InstanceStatus.Inactive),
+      new EquipmentInstance(uuid(), uuid(), 'instance3', 'description of instance 3', InstanceStatus.Active),
+      new EquipmentInstance(uuid(), uuid(), 'instance4', 'description of instance 4', InstanceStatus.Decommissioned),
+      new EquipmentInstance(uuid(), uuid(), 'instance5', 'description of instance 5', InstanceStatus.Active),
+      new EquipmentInstance(uuid(), uuid(), 'instance1', 'description of instance 1', InstanceStatus.Active),
+      new EquipmentInstance(uuid(), uuid(), 'instance2', 'description of instance 2', InstanceStatus.Inactive),
+      new EquipmentInstance(uuid(), uuid(), 'instance3', 'description of instance 3', InstanceStatus.Active),
+      new EquipmentInstance(uuid(), uuid(), 'instance4', 'description of instance 4', InstanceStatus.Decommissioned),
+      new EquipmentInstance(uuid(), uuid(), 'instance5', 'description of instance 5', InstanceStatus.Active),
+    ]);
+  }
+}
--- monitoring-client/src/app/equipment-models/equipment-models.module.ts
+++ monitoring-client/src/app/equipment-models/equipment-models.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
-import { EquipmentModelsComponent } from './equipment-models.component';
+import { EquipmentModelsTableComponent } from './equipment-models-table/equipment-models-table.component';
+import { EquipmentModelInfoComponent } from './equipment-model-info/equipment-model-info.component';
 import { EquipmentModelsRoutingModule } from './equipment-models-routing.module';
 
 import { PipeModule } from '../pipes/pipes.module';
 
 @NgModule({
-  imports: [CommonModule, EquipmentModelsRoutingModule, PipeModule],
-  declarations: [EquipmentModelsComponent],
+  imports: [CommonModule, EquipmentModelsRoutingModule, PipeModule, HttpClientModule],
+  declarations: [EquipmentModelsTableComponent, EquipmentModelInfoComponent],
 })
 export class EquipmentModelsModule {}

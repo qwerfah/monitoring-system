07fc3d43516ed85ca3a98038f317c852ca83c1e1
qwerfah <qwerfah@gmail.com>
2021-10-04 10:56:03 +0300

    Equipment models table updated

--- monitoring-client/src/app/equipment-models/equipment-models-table/equipment-models-table.component.ts
+++ monitoring-client/src/app/equipment-models/equipment-models-table/equipment-models-table.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
-import { EquipmentModel } from '../models/equipment-model';
+import { EquipmentModel } from '../../models/equipment-model';
 
 import { v4 as uuid } from 'uuid';
 
 @Component({
   selector: 'app-equipment-models',
-  templateUrl: './equipment-models.component.html',
-  styleUrls: ['./equipment-models.component.css'],
+  templateUrl: './equipment-models-table.component.html',
+  styleUrls: ['./equipment-models-table.component.css'],
 })
-export class EquipmentModelsComponent implements OnInit {
+export class EquipmentModelsTableComponent implements OnInit {
   models: EquipmentModel[] = [
+    new EquipmentModel(uuid(), 'model_1', 'description_1'),
+    new EquipmentModel(uuid(), 'model_1', 'description_1'),
+    new EquipmentModel(uuid(), 'model_1', 'description_1'),
+    new EquipmentModel(uuid(), 'model_1', 'description_1'),
+    new EquipmentModel(uuid(), 'model_1', 'description_1'),
+    new EquipmentModel(uuid(), 'model_1', 'description_1'),
+    new EquipmentModel(uuid(), 'model_1', 'description_1'),
+    new EquipmentModel(uuid(), 'model_1', 'description_1'),
+    new EquipmentModel(uuid(), 'model_1', 'description_1'),
+    new EquipmentModel(uuid(), 'model_1', 'description_1'),
+    new EquipmentModel(uuid(), 'model_1', 'description_1'),
+    new EquipmentModel(uuid(), 'model_1', 'description_1'),
     new EquipmentModel(uuid(), 'model_1', 'description_1'),
     new EquipmentModel(uuid(), 'model_1', 'description_1'),
     new EquipmentModel(uuid(), 'model_1', 'description_1'),

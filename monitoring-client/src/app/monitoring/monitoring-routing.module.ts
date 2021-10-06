825472ee917c04cc444c328004bfc30935274a69
qwerfah <qwerfah@gmail.com>
2021-10-06 23:06:33 +0300

    Monitoring routing module created

---
+++ monitoring-client/src/app/monitoring/monitoring-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AuthGuard } from '../helpers/auth.guard';
+import { MonitorInfoComponent } from './monitor-info/monitor-info.component';
+import { MonitorsTableComponent } from './monitors-table/monitors-table.component';
+
+const routes: Routes = [
+  {
+    path: 'monitors',
+    component: MonitorsTableComponent,
+    /*
+    canActivate: [AuthGuard],
+    data: {
+      role: 'EquipmentUser',
+    },
+    */
+  },
+  {
+    path: 'monitors/:uid',
+    component: MonitorInfoComponent,
+    /*
+    canActivate: [AuthGuard],
+    data: {
+      role: 'EquipmentUser',
+    },
+    */
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class MonitoringRoutingModule {}
--- monitoring-client/src/app/monitoring/monitoring.module.ts
+++ monitoring-client/src/app/monitoring/monitoring.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MonitoringComponent } from './monitoring.component';
+
+import { MonitorsTableComponent } from './monitors-table/monitors-table.component';
+import { MonitorInfoComponent } from './monitor-info/monitor-info.component';
+import { CreateMonitorComponent } from './create-monitor/create-monitor.component';
+import { MonitoringRoutingModule } from './monitoring-routing.module';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [MonitoringComponent]
+  imports: [CommonModule, MonitoringRoutingModule],
+  declarations: [MonitorsTableComponent, MonitorInfoComponent, CreateMonitorComponent],
 })
-export class MonitoringModule { }
+export class MonitoringModule {}

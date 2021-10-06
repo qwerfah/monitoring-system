f37624b0462d06df22ad337f67009c006f04983c
qwerfah <qwerfah@gmail.com>
2021-10-06 21:39:08 +0300

    Frontend session service implemented

--- monitoring-client/src/app/models/user.ts
+++ monitoring-client/src/app/models/user.ts
@@ -1,3 +1,5 @@
+import { Token } from "./token";
+
 export class User {
-  constructor(public uid: string, public login: string) {}
+  constructor(public uid: string, public login: string, public token: Token | null) {}
 }
--- monitoring-client/src/app/services/session.service.ts
+++ monitoring-client/src/app/services/session.service.ts
@@ -1,20 +1,74 @@
 import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 import { Credentials } from '../models/credentials';
-import { Observable } from 'rxjs';
+import { User } from '../models/user';
+import { Token } from '../models/token';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SessionService {
+  private currentUserSubject: BehaviorSubject<User | null>;
+  public currentUser$: Observable<User>;
+
   constructor(private http: HttpClient, @Inject('GATEWAY_URI') private gatewayUri: string) {}
 
-  login(creds: Credentials) {}
+  /** Login in gateway service with given credentials. Recieved user data is stored in local storage.
+   * @param creds User credentials.
+   * @returns Observable instance with logged in user instance contains new access and refresh tokens.
+   */
+  login(creds: Credentials): Observable<User> {
+    return this.http
+      .post<User>(`${this.gatewayUri}/api/session/login`, { params: { login: creds.login, password: creds.password } })
+      .pipe(
+        map((user) => {
+          if (user && user.token && user.token.access) {
+            localStorage.setItem('currentUser', JSON.stringify(user));
+            this.currentUserSubject.next(user);
+          }
 
-  logout() {}
+          return user;
+        })
+      );
+  }
 
-  refresh() {}
+  /** Refresh access and refresh tokens with refresh token for current logged in user.
+   * @returns Observable with new user tokens.
+   */
+  refresh(): Observable<Token> {
+    return this.http
+      .post<Token>('${this.gatewayUri}/api/session/refresh', null, {
+        headers: { Authorization: `Bearer ${this.currentUserSubject.value?.token?.refresh}` },
+      })
+      .pipe(tap((tokens) => this.updateTokens(tokens)));
+  }
 
-  register() {}
+  /** Remove current user data from local storage. */
+  logout(): void {
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
+
+  /** Register new user using gateway service. Doesn't login
+   * created user and doesn't update current user data in local storage.
+   * @param user New user data.
+   * @returns Observable with registered user data.
+   */
+  register(user: User): Observable<User> {
+    return this.http.post<User>('${this.gatewayUri/session/register}', user);
+  }
+
+  /** Update user tokens for user in local storage.
+   * @param token New access and refresh tokens.
+   */
+  private updateTokens(token: Token) {
+    let user = this.currentUserSubject.value;
+    if (user != null) {
+      user.token = token;
+      localStorage.setItem('currentUser', JSON.stringify(user));
+    }
+  }
 }

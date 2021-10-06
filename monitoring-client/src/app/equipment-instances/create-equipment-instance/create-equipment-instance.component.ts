af89f8358301f9bdfa226d01424f89d71980e925
qwerfah <qwerfah@gmail.com>
2021-10-07 00:03:14 +0300

    Modal window for adding new instance updated

--- monitoring-client/src/app/equipment-instances/create-equipment-instance/create-equipment-instance.component.ts
+++ monitoring-client/src/app/equipment-instances/create-equipment-instance/create-equipment-instance.component.ts
@@ -1,6 +1,8 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { Component, EventEmitter, OnInit, Output, QueryList, ViewChildren } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatMenuTrigger } from '@angular/material/menu';
+import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 import { EquipmentInstance } from 'src/app/models/equipment-instance';
 import { EquipmentModel } from 'src/app/models/equipment-model';
@@ -17,35 +19,59 @@ export class CreateEquipmentInstanceComponent implements OnInit {
 
   InstanceStatus = InstanceStatus;
 
-  modelForm: FormGroup;
+  instanceForm: FormGroup;
+
   status: InstanceStatus;
+  selectedModel: EquipmentModel;
 
   models$: Observable<EquipmentModel[]>;
 
+  @ViewChildren(MatMenuTrigger) trigger: QueryList<MatMenuTrigger>;
+
   constructor(private fb: FormBuilder, private equipmentService: EquipmentService) {
-    this.modelForm = fb.group({
+    this.instanceForm = fb.group({
       name: [null, [Validators.required, Validators.minLength(3)]],
       description: [null, [Validators.minLength(0)]],
     });
 
-    this.models$ = equipmentService.getModels();
+    this.models$ = of([
+      new EquipmentModel('', 'model_1', 'description_1'),
+      new EquipmentModel('', 'model_2', 'description_2'),
+      new EquipmentModel('', 'model_3', 'description_3'),
+      new EquipmentModel('', 'model_4', 'description_4'),
+      new EquipmentModel('', 'model_5', 'description_5'),
+    ]);
+
     this.status = InstanceStatus.Active;
+    this.selectedModel = new EquipmentModel('', '', '');
   }
 
   ngOnInit() {}
 
-  setStatus(status: InstanceStatus) {
+  openStatusMenu() {
+    this.trigger.toArray()[0].openMenu();
+  }
+
+  openModelsMenu() {
+    this.trigger.toArray()[1].openMenu();
+  }
+
+  setStatus(status: InstanceStatus): void {
     this.status = status;
   }
 
+  setModel(model: EquipmentModel): void {
+    this.selectedModel = model;
+  }
+
   submit() {
-    if (this.modelForm.invalid) {
-      this.modelForm.markAllAsTouched();
+    if (this.instanceForm.invalid) {
+      this.instanceForm.markAllAsTouched();
     } else {
       this.addEvent.emit(new EquipmentInstance('', '', 'name', 'desc', InstanceStatus.Active));
 
-      console.log(`name: ${this.modelForm.controls.name.value}`);
-      console.log(`description: ${this.modelForm.controls.description.value}`);
+      console.log(`name: ${this.instanceForm.controls.name.value}`);
+      console.log(`description: ${this.instanceForm.controls.description.value}`);
     }
   }
 

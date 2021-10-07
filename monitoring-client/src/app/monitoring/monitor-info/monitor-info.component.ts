b9dcb0cf6b8f32f10b9abc3cc7d43cab7307b364
qwerfah <qwerfah@gmail.com>
2021-10-07 17:18:53 +0300

    Chart added to monitor info page

--- monitoring-client/src/app/monitoring/monitor-info/monitor-info.component.ts
+++ monitoring-client/src/app/monitoring/monitor-info/monitor-info.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { v4 as uuid } from 'uuid';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 import { Monitor } from 'src/app/models/monitor';
 import { Param } from 'src/app/models/param';
+import { ParamValue } from 'src/app/models/param-value';
 
 @Component({
   selector: 'app-monitor-info',
@@ -14,6 +16,7 @@ export class MonitorInfoComponent implements OnInit {
   monitorUid: string;
   monitor$: Observable<Monitor>;
   params$: Observable<Param[]>;
+  paramValues$: Observable<ParamValue[]>;
 
   constructor() {
     this.monitor$ = of(new Monitor(uuid(), uuid(), uuid(), 'monitor_1', 'instance_1', 'model_1', 'description'));
--- monitoring-client/src/app/monitoring/monitoring.module.ts
+++ monitoring-client/src/app/monitoring/monitoring.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { MatMenuModule } from '@angular/material/menu';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MonitorsTableComponent } from './monitors-table/monitors-table.component';
-import { MonitorInfoComponent } from './monitor-info/monitor-info.component';
 import { CreateMonitorComponent } from './create-monitor/create-monitor.component';
+import { MonitorInfoComponent } from './monitor-info/monitor-info.component';
 import { MonitoringRoutingModule } from './monitoring-routing.module';
 import { PipeModule } from '../pipes/pipes.module';
 
 @NgModule({
-  imports: [CommonModule, MonitoringRoutingModule, PipeModule, FormsModule, ReactiveFormsModule, MatMenuModule],
+  imports: [
+    CommonModule,
+    MonitoringRoutingModule,
+    PipeModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatMenuModule,
+    NgxChartsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+  ],
   declarations: [MonitorsTableComponent, MonitorInfoComponent, CreateMonitorComponent],
 })
 export class MonitoringModule {}

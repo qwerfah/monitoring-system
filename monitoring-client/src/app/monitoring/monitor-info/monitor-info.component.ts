52546ad933ada57adf615cb37de1825ed9623ee0
qwerfah <qwerfah@gmail.com>
2021-10-07 16:33:44 +0300

    Monitor info page added

--- monitoring-client/src/app/monitoring/monitor-info/monitor-info.component.ts
+++ monitoring-client/src/app/monitoring/monitor-info/monitor-info.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { v4 as uuid } from 'uuid';
+
+import { Monitor } from 'src/app/models/monitor';
+import { Param } from 'src/app/models/param';
 
 @Component({
   selector: 'app-monitor-info',
   templateUrl: './monitor-info.component.html',
-  styleUrls: ['./monitor-info.component.css']
+  styleUrls: ['./monitor-info.component.css'],
 })
 export class MonitorInfoComponent implements OnInit {
+  monitorUid: string;
+  monitor$: Observable<Monitor>;
+  params$: Observable<Param[]>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.monitor$ = of(new Monitor(uuid(), uuid(), uuid(), 'monitor_1', 'instance_1', 'model_1', 'description'));
+    this.params$ = of([
+      new Param(uuid(), uuid(), 'Length', 'm'),
+      new Param(uuid(), uuid(), 'Heigth', 'm'),
+      new Param(uuid(), uuid(), 'Width', 'm'),
+      new Param(uuid(), uuid(), 'Weigth', 'kg'),
+      new Param(uuid(), uuid(), 'Speed', 'm/s'),
+    ]);
   }
 
+  ngOnInit() {}
 }

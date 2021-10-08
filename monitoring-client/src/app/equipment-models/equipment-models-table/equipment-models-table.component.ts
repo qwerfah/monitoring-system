09ac2ae60341a3886f28bf052664afce26afb69c
qwerfah <qwerfah@gmail.com>
2021-10-09 01:29:07 +0300

    Models table component now uses recieve data from server

--- monitoring-client/src/app/equipment-models/equipment-models-table/equipment-models-table.component.ts
+++ monitoring-client/src/app/equipment-models/equipment-models-table/equipment-models-table.component.ts
@@ -3,6 +3,10 @@ import { Component, OnInit } from '@angular/core';
 import { EquipmentModel } from '../../models/equipment-model';
 
 import { v4 as uuid } from 'uuid';
+import { Observable } from 'rxjs';
+import { EquipmentService } from 'src/app/services/equipment.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-equipment-models',
@@ -11,195 +15,36 @@ import { v4 as uuid } from 'uuid';
 })
 export class EquipmentModelsTableComponent implements OnInit {
   isAdding: boolean = false;
-  models: EquipmentModel[] = [
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-    new EquipmentModel(
-      uuid(),
-      'model_1 model_1 model_1',
-      'description_1 description_1 description_1 description_1 description_1'
-    ),
-  ];
+  isLoading: boolean = true;
+  models: EquipmentModel[];
 
-  constructor() {}
+  constructor(private equipmentService: EquipmentService, private snackBar: MatSnackBar) {
+    equipmentService.getModels().subscribe(
+      (models) => {
+        this.models = models;
+        this.isLoading = false;
+      },
+      (err: HttpErrorResponse) => {
+        switch (err.status) {
+          case 0: {
+            this.snackBar.open('Ошибка: отсутсвтует соединение с сервером', 'Ок');
+            break;
+          }
+          case 502: {
+            this.snackBar.open('Ошибка: сервис оборудования недоступен', 'Ок');
+            break;
+          }
+          case 404: {
+            this.snackBar.open('Ошибка: данные не найдены', 'Ок');
+          }
+        }
+        this.isLoading = false;
+      },
+      () => {
+        this.isLoading = false;
+      }
+    );
+  }
 
   ngOnInit() {}
 

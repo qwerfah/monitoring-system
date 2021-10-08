fdc39b67b2b7a1ac25b57768b042624ca9ee36fc
qwerfah <qwerfah@gmail.com>
2021-10-09 01:30:02 +0300

    Models module dependencies updated

--- monitoring-client/src/app/equipment-models/equipment-models.module.ts
+++ monitoring-client/src/app/equipment-models/equipment-models.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { EquipmentModelsTableComponent } from './equipment-models-table/equipment-models-table.component';
 import { EquipmentModelInfoComponent } from './equipment-model-info/equipment-model-info.component';
@@ -9,9 +10,19 @@ import { CreateEquipmentModelComponent } from './create-equipment-model/create-e
 import { EquipmentModelsRoutingModule } from './equipment-models-routing.module';
 
 import { PipeModule } from '../pipes/pipes.module';
+import { GeneralModule } from '../general/general.module';
 
 @NgModule({
-  imports: [CommonModule, EquipmentModelsRoutingModule, PipeModule, HttpClientModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    EquipmentModelsRoutingModule,
+    PipeModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    GeneralModule
+  ],
   declarations: [EquipmentModelsTableComponent, EquipmentModelInfoComponent, CreateEquipmentModelComponent],
 })
 export class EquipmentModelsModule {}

735f3eda262f3835397544a048c5433d90fc2661
qwerfah <qwerfah@gmail.com>
2021-10-09 01:30:58 +0300

    Instances module dependencies updated

--- monitoring-client/src/app/equipment-instances/equipment-instances.module.ts
+++ monitoring-client/src/app/equipment-instances/equipment-instances.module.ts
@@ -4,11 +4,13 @@ import { PipeModule } from '../pipes/pipes.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { EquipmentInstancesRoutingModule } from './equipment-instances-routing.module';
 import { EquipmentInstanceInfoComponent } from './equipment-instance-info/equipment-instance-info.component';
 import { EquipmentInstancesTableComponent } from './equipment-instances-table/equipment-instances-table.component';
 import { CreateEquipmentInstanceComponent } from './create-equipment-instance/create-equipment-instance.component';
+import { GeneralModule } from '../general/general.module';
 
 @NgModule({
   imports: [
@@ -19,6 +21,8 @@ import { CreateEquipmentInstanceComponent } from './create-equipment-instance/cr
     FormsModule,
     ReactiveFormsModule,
     MatMenuModule,
+    MatSnackBarModule,
+    GeneralModule,
   ],
   declarations: [EquipmentInstancesTableComponent, EquipmentInstanceInfoComponent, CreateEquipmentInstanceComponent],
 })

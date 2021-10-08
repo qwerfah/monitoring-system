42d8f199c2c0d7bc5afecc47c4e53c1c986f9e5b
qwerfah <qwerfah@gmail.com>
2021-10-09 01:27:24 +0300

    Auth guard fixed

--- monitoring-client/src/app/helpers/auth.guard.ts
+++ monitoring-client/src/app/helpers/auth.guard.ts
@@ -12,7 +12,7 @@ export class AuthGuard implements CanActivate {
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     const currentUser = this.sessionService.currentUser;
 
-    if (currentUser) {
+    if (currentUser !== undefined) {
       if (route.data.roles && route.data.roles.indexOf(currentUser.role) === -1) {
         this.router.navigate(['/']);
         return false;

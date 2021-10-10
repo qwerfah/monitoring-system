46b0f1baf361eabaabcc9a35103f528ffe55167c
qwerfah <qwerfah@gmail.com>
2021-10-10 12:28:24 +0300

    Routing updated

--- monitoring-client/src/app/monitoring/monitoring.module.ts
+++ monitoring-client/src/app/monitoring/monitoring.module.ts
@@ -4,6 +4,7 @@ import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { MatMenuModule } from '@angular/material/menu';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MonitorsTableComponent } from './monitors-table/monitors-table.component';
@@ -11,6 +12,7 @@ import { CreateMonitorComponent } from './create-monitor/create-monitor.componen
 import { MonitorInfoComponent } from './monitor-info/monitor-info.component';
 import { MonitoringRoutingModule } from './monitoring-routing.module';
 import { PipeModule } from '../pipes/pipes.module';
+import { GeneralModule } from '../general/general.module';
 
 @NgModule({
   imports: [
@@ -23,6 +25,8 @@ import { PipeModule } from '../pipes/pipes.module';
     NgxChartsModule,
     BrowserModule,
     BrowserAnimationsModule,
+    MatSnackBarModule,
+    GeneralModule,
   ],
   declarations: [MonitorsTableComponent, MonitorInfoComponent, CreateMonitorComponent],
 })
